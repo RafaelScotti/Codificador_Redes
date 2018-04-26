14284713fd86c5c77f945482a6a6421ec6356e7d
RafaelScotti <rafael.scotti@acad.pucrs.br>
2018-04-26 19:04:48 -0300

    Funções definidas Em desenvolvimento

--- Codificador.py
+++ Codificador.py
@@ -19,75 +19,78 @@ else:
     print("Vc deve digitar dois argumentos <tecnica codificacao> <valor hexa>")
     sys.exit()
 
-
 t = len(valorHexa)*4 #verifica total de bits
 convertBin = (bin(int(valorHexa, 16))[2:]).zfill(t)#Converte para binario
 print(convertBin)
-#======DEFINIR FUNCAO NRZI======
-saidaNRZI=""
-lastSinal = "-"
-if(tecnica=="nrzi"):
+
+#FUNCAO NRZI
+def nrzi(convertBin):
+    saidaNRZI=""
+    lastSinal = "-"
+    #if(tecnica=="nrzi"):
     for n in convertBin:
         if(n=="1"):
-            if(lastSinal=="+"):    
+            if(lastSinal=="+"):
                 saidaNRZI = saidaNRZI + "-"
                 lastSinal="-"
             else:
                 saidaNRZI = saidaNRZI + "+"
                 lastSinal="+"
-            
+
         elif(n=="0"):
             saidaNRZI = saidaNRZI + lastSinal
 
-print(saidaNRZI)
+    print(saidaNRZI)
 
-#=====FIM CODIFICADOR NRZI======
+#UNCAO MANCH
+def manch(convertBin):
+    saidaMANCH=""
+    if(tecnica=="manch"):
+        for n in convertBin:
+            if(n=="1"):
+                saidaMANCH = saidaMANCH + "-+"
+            elif(n=="0"):
+                saidaMANCH = saidaMANCH + "+-"
+            #saidaMANCH +="
 
-#=====DEFINIR FUNCAO MANCH======
-saidaMANCH=""
-if(tecnica=="manch"):
-    for n in convertBin:
-        if(n=="1"):
-            saidaMANCH = saidaMANCH + "-+"
-        elif(n=="0"):
-            saidaMANCH = saidaMANCH + "+-"
- 
-print(saidaMANCH)
-#=====FIM CODIFICADOR MANCH======
+    print(saidaMANCH)
 
-#=====DEFINIR FUNCAO MLT-3=======
+#FUNCAO MLT3
+def mlt3(convertBin):
+    saidaMLT3=""
+    ultimoSinal="0"
+    penultimoSinal="-"
+    if(tecnica=="mlt3"):
+        for n in convertBin:
+            if(n=="1" and penultimoSinal=="-" and ultimoSinal=="0"):
+                saidaMLT3=saidaMLT3 + "+"
+                penultimoSinal = ultimoSinal
+                ultimoSinal="+"
+            elif(n=="1" and penultimoSinal=="0" and ultimoSinal=="+"):
+                saidaMLT3  += "0"
+                penultimoSinal = ultimoSinal
+                ultimoSinal = "0"
+            elif(n=="1" and penultimoSinal=="0" and ultimoSinal=="-"):
+                saidaMLT3+="0"
+                penultimoSinal = ultimoSinal
+                ultimoSinal = "0"
+            elif(n=="1" and penultimoSinal=="+" and ultimoSinal=="0"):
+                saidaMLT3+="-"
+                penultimoSinal = ultimoSinal
+                ultimoSinal="-"
+            elif(n=="0"):
+                saidaMLT3+=ultimoSinal
+            #saidaMLT3+=" "
+    print(saidaMLT3)
 
-saidaMLT3=""
-ultimoSinal="0"
-penultimoSinal="-1"
-if(tecnica=="mlt3"):
-    for n in convertBin:
-        if(n=="1" and penultimoSinal=="-1" and ultimoSinal=="0"):
-            saidaMLT3=saidaMLT3 + "+1"
-            penultimoSinal = ultimoSinal
-            ultimoSinal="+1"
-        elif(n=="1" and penultimoSinal=="0" and ultimoSinal=="+1"):
-            saidaMLT3  += "0"
-            penultimoSinal = ultimoSinal
-            ultimoSinal = "0"
-        elif(n=="1" and penultimoSinal=="0" and ultimoSinal=="-1"):
-            saidaMLT3+="0"
-            penultimoSinal = ultimoSinal
-            ultimoSinal = "0"
-        elif(n=="1" and penultimoSinal=="+1" and ultimoSinal=="0"):
-            saidaMLT3+="-1"
-            penultimoSinal = ultimoSinal
-            ultimoSinal="-1"
-        elif(n=="0"):
-            saidaMLT3+=ultimoSinal
-
-print(saidaMLT3)
-#APAGAR TUDO
-#CRIA UMA VARIAVEL Q ARMAZENA O ULTIMO SINAL (QUE NAO SEJA O "N") E ALTERAR ELA DEPENDENDO
-#DO VALOR LIDO NO "N"
-
-
+#FUNCAO 8B10B
+def t8b10b(convertBin):
+    print("Em desenvolvimento!")
 
 
-
-
+if(tecnica=="nrzi"):
+    nrzi(convertBin)
+elif(tecnica=="manch"):
+    manch(convertBin)
+elif(tecnica=="mlt3"):
+    mlt3(convertBin)
