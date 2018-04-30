ec4bd0070ae37c7d74550dadc22ba6a12422c871
RafaelScotti <rafael.scotti@acad.pucrs.br>
2018-04-30 16:40:16 -0300

    decodificador mlt3 funcionando

--- Decodificador.py
+++ Decodificador.py
@@ -2,8 +2,8 @@ import sys
 import string
 import binascii
 
-#sinais = "---+++----+--+++"
-sinais = "+--+"
+sinais = "---+++----+--+++"
+#sinais = "+++000-0"
 tecnica = "manch"
 
 def convert(binSring):
@@ -28,7 +28,7 @@ def decodeNRZI(sinais):
 def decodeMANCH(sinais):
     saidaMANCH = ""
     cont = 0
-    if(len(sinais) % 2 ==0 and sinais!=""):
+    if(len(sinais) % 2 == 0 and sinais!=""):
         while(cont <= len(sinais)-2):
             if(sinais[cont]==sinais[cont+1]):
                 if(saidaMANCH!=""):
@@ -48,9 +48,55 @@ def decodeMANCH(sinais):
         print("Erro, numero de sinais incorreto!")
 
 
+def decodeMLT3(sinais):
+    saidaMLT3 = ""
+    us = "0"
+    sa = "-"
+    for n in sinais:
+        if(n=="0"):
+            if(us=="0"):
+                saidaMLT3+="0"
+            else:
+                if(us=="+" and (sa=="-" or sa=="0")):
+                    saidaMLT3+="1"
+                    us="0"
+                    sa="+"
+                elif(us=="-" and (sa=="+" or sa=="0")):
+                    saidaMLT3+="1"
+                    us="0"
+                    sa="-"
+                else:
+                    print("Erro")
+                    sys.exit()
+        elif(n=="+"):
+            if(us=="+"):
+                saidaMLT3+="0"
+            else:
+                if(us=="0" and sa=="-"):
+                    saidaMLT3+="1"
+                    us="+"
+                    sa="0"
+                else:
+                    print("Erro")
+                    sys.exit()
+        elif(n=="-"):
+            if(us=="-"):
+                saidaMLT3+="0"
+            else:
+                if(us=="0" and sa=="+"):
+                    saidaMLT3+="1"
+                    us="-"
+                    sa="0"
+                else:
+                    print("Erro")
+                    sys.exit()
+        else:
+            print("sinal invalido")
+            sys.exit()
+    print(convert(saidaMLT3))
 
 
 #decodeNRZI(sinais)
-decodeMANCH(sinais)
-#decodeMLT3(sinais)
+#decodeMANCH(sinais)
+decodeMLT3(sinais)
 #decodeT8B10B(sinais)
