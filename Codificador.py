3a43ddbbb3d82c34835c88e58c46f14b33bc9bec
RafaelScotti <rafael.scotti@acad.pucrs.br>
2018-05-02 01:11:45 -0300

    8b10b pronto

--- Codificador.py
+++ Codificador.py
@@ -2,7 +2,7 @@ import sys
 import string
 
 #FUNCAO NRZI
-def nrzi(convertBin):
+def encodeNRZI(convertBin):
     saidaNRZI=""
     lastSinal = "-"
     #if(tecnica=="nrzi"):
@@ -21,7 +21,7 @@ def nrzi(convertBin):
     print(saidaNRZI)
 
 #FUNCAO MANCH
-def manch(convertBin):
+def encodeMANCH(convertBin):
     saidaMANCH=""
     if(tecnica=="manch"):
         for n in convertBin:
@@ -34,7 +34,7 @@ def manch(convertBin):
     print(saidaMANCH)
 
 #FUNCAO MLT3
-def mlt3(convertBin):
+def encodeMLT3(convertBin):
     saidaMLT3=""
     ultimoSinal="0"
     penultimoSinal="-"
@@ -63,139 +63,146 @@ def mlt3(convertBin):
 
 
 #FUNCAO 8B10B
-def t8b10b(convertBin):
+def encode8B10B(convertBin):
     while((len(convertBin)%8)!=0):
         convertBin="0"+convertBin
-    print(convertBin)
-
+    #print(convertBin)
+    rd = "-1"    
     saida8B10B = ""
-    rd = "-1"
-    v34 = ""
-    v56 = ""
     cont = 0
-    #contV = 3 #ate 3
-    
-
     while(cont<len(convertBin)):
-        v34 = convertBin[cont:cont+3]
-        cont+=3
-        v56 = convertBin[cont:cont+5]
-        cont+=5
-        #contV += cont
-        #print("."+v56+".\n")
-        saida8B10B+= tabela5B6B(v56, rd) + tabela3B4B(v34, rd)+" "
+        bits = ""
+        contBits = 0
+        while(contBits<8):
+            bits+=convertBin[cont]
+            cont+=1
+            contBits+=1
+        #cont+=1
+        #print(bits)
+        saida8B10B+=tabela8B10B(bits, rd)+" "
         if(rd=="-1"):
             rd="+1"
         else:
-            rd="-1"
-    #print("."+v34+".\n"+"."+v56+".")
-    #print(v34, v56)
-    print("\n"+saida8B10B)
-    print(len(convertBin))
+            rd="-1"    
+    encodeNRZI(saida8B10B)
 
-#vou mudar sapoha toda
-def tabela5B6B(v56, rd):
-    if(v56=="00000"):         #0
-        return "100111" if rd=="-1" else "011000"
+def tabela8B10B(bits, rd):
+    v34 = bits[0:3]
+    v56 = bits[3:8]
+    #print("."+v56+".")
+    saida = ""
+
+    #tabela 5b10b
+    if(v56=="00000"):           #0
+        saida = "100111" if rd=="-1" else "011000"
     elif(v56=="00001"):         #1
-        return "011101" if rd=="-1" else "100010"
+        saida = "011101" if rd=="-1" else "100010"
     elif(v56=="00010"):         #2
-        return "101101" if rd=="-1" else "010010"
+        saida = "101101" if rd=="-1" else "010010"
     elif(v56=="00011"):         #3
-        return "110001"
+        saida = "110001"
     elif(v56=="00100"):         #4
-        return "110101" if rd=="-1" else "001010"
+        saida = "110101" if rd=="-1" else "001010"
     elif(v56=="00101"):         #5
-        return "101001"
+        saida = "101001"
     elif(v56=="00110"):         #6
-        return "011001"
+        saida = "011001"
     elif(v56=="00111"):         #7
-        return "111000" if rd=="-1" else "000111"
+        saida = "111000" if rd=="-1" else "000111"
     elif(v56=="01000"):         #8
-        return "111001" if rd=="-1" else "000110"
+        saida = "111001" if rd=="-1" else "000110"
     elif(v56=="01001"):         #9
-        return "100101"
+        saida = "100101"
     elif(v56=="01010"):         #10
-        return "010101"
+        saida = "010101"
     elif(v56=="01011"):         #11
-        return "110100"
+        saida = "110100"
     elif(v56=="01100"):         #12
-        return "001101"
+        saida = "001101"
     elif(v56=="01101"):         #13
-        return "101100"
+        saida = "101100"
     elif(v56=="01110"):         #14
-        return "011100"
+        saida = "011100"
     elif(v56=="01111"):         #15
-        return "010111" if rd=="-1" else "101000"
+        saida = "010111" if rd=="-1" else "101000"
     elif(v56=="10000"):         #16
-        return "011011" if rd=="-1" else "100100"
+        saida = "011011" if rd=="-1" else "100100"
     elif(v56=="10001"):         #17
-        return "100011"
+        saida = "100011"
     elif(v56=="10010"):         #18
-        return "010011"
+        saida = "010011"
     elif(v56=="10011"):         #19
-        return "110010"
+        saida = "110010"
     elif(v56=="10100"):         #20
-        return "001011"
+        saida = "001011"
     elif(v56=="10101"):         #21
-        return "101010"
+        saida = "101010"
     elif(v56=="10110"):         #22
-        return "011010"
+        saida = "011010"
     elif(v56=="10111"):         #23
-        return "111010" if rd=="-1" else "000101"
+        saida = "111010" if rd=="-1" else "000101"
     elif(v56=="11000"):         #24
-        return "110011" if rd=="-1" else "001100"
+        saida = "110011" if rd=="-1" else "001100"
     elif(v56=="11001"):         #25
-        return "100110"
+        saida = "100110"
     elif(v56=="11010"):         #26
-        return "010110"
+        saida = "010110"
     elif(v56=="11011"):         #27
-        return "110110" if rd=="-1" else "001001"
+        saida = "110110" if rd=="-1" else "001001"
     elif(v56=="11100	"):         #28
-        return "001110"
+        saida = "001110"
     elif(v56=="11101"):         #29
-        return "101110" if rd=="-1" else "010001"
+        saida = "101110" if rd=="-1" else "010001"
     elif(v56=="11110"):         #30
-        return "011110" if rd=="-1" else "100001"
+        saida = "011110" if rd=="-1" else "100001"
     elif(v56=="11111"):         #31
-        return "101011" if rd=="-1" else "010100"
+        saida = "101011" if rd=="-1" else "010100"
     else:
         return "\nErro: Valores invalidos"
-    return "ok"
+    
 
-def tabela3B4B(v34, rd):
+    #tabela 3b4b
     if(v34=="000"):             #0
-        return "1011" if rd=="-1" else "0100"
-        #if(rd=="-1"):
-        #    return "1011"
-       # else:
-        #    return "0100"
+        saida += "1011" if rd=="-1" else "0100"
     elif(v34=="001"):           #1
-        return "1001"
+        saida += "1001"
     elif(v34=="010"):           #2
-        return "0101"
+        saida += "0101"
     elif(v34=="011"):           #3
         if(rd=="-1"):
-            return "1100"
+            saida += "1100"
         else:
-            return "0011"
+            saida += "0011"
     elif(v34=="100"):           #4
         if(rd=="-1"):
-            return "1101"
+            saida += "1101"
         else: 
-            return "0010"
+            saida += "0010"
     elif(v34=="101"):           #5
-        return "1010"
+        saida += "1010"
     elif(v34=="110"):           #6
-        return "0110"
-    elif(v34=="111"):           #7 - falta implementar as outras formas
-        if(rd=="-1"):
-            return "1110"
-        else:
-            return "0001"
+        saida += "0110"
+    elif(v34=="111"):           #7 
+        if(rd=="+1"):
+            if(v56=="01011" or v56=="01101" or v56=="01110"): #D.x.A7 - x11, x13, x14 RD +1
+                saida += "1000"
+                #print("D.x.A7 - x11, x13, x14 RD +1")
+            else:
+                saida += "0001" #D.x.P7
+                #print("P7+")
+        elif(rd=="-1"):
+            if(v56=="10001" or v56=="10010" or v56=="10100"): #D.x.A7 - x17, x18, x20 RD -1
+                saida += "0111"
+                #print("D.x.A7 - x17, x18, x20 RD -1")
+            else:
+                saida += "1110" #D.x.P7
+                #print("P7-")
     else:
         return "\nErro: Valores invalidos"
-    return "ok"
+    
+    return saida
+
+
 
 #===main===
 #variaveis utilizadas
@@ -220,13 +227,13 @@ convertBin = (bin(int(valorHexa, 16))[2:]).zfill(t)#Converte para binario
 
 
 if(tecnica=="nrzi"):
-    nrzi(convertBin)
+    encodeNRZI(convertBin)
 elif(tecnica=="manch"):
-    manch(convertBin)
+    encodeMANCH(convertBin)
 elif(tecnica=="mlt3"):
-    mlt3(convertBin)
+    encodeMLT3(convertBin)
 elif(tecnica=="8b10b"):
-    t8b10b(convertBin)
+    encode8B10B(convertBin)
 else:
     print("Erro: Tecnica invalida")
     sys.exit()
